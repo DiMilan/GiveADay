af73191a5d5d98e7848ac133ff6e993ef432541e
Bart Vanmarcke <bart@vanmarcke-software.be>
2017-02-25 12:41:17 +0100

    GroupController: Add Create function

--- src/GoedBezigWebApp/Controllers/GroupController.cs
+++ src/GoedBezigWebApp/Controllers/GroupController.cs
@@ -4,12 +4,20 @@ using System.Linq;
 using System.Threading.Tasks;
 using GoedBezigWebApp.Models;
 using GoedBezigWebApp.Models.GroupViewModels;
+using GoedBezigWebApp.Models.Repositories;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GoedBezigWebApp.Controllers
 {
     public class GroupController : Controller
     {
+        private readonly IGroupRepository _groupRepository;
+
+        public GroupController(IGroupRepository groupRepository)
+        {
+            _groupRepository = groupRepository;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -19,6 +27,34 @@ namespace GoedBezigWebApp.Controllers
         {
             Group group = new Group();
             return View(new GroupEditViewModel(group));
+        }
+
+        public IActionResult Create()
+        {
+            return View(nameof(Edit), new GroupEditViewModel(new Group()));
+        }
+
+        [HttpPost]
+        public IActionResult Create(GroupEditViewModel groupEditViewModel)
+        {
+            Group group = new Group();
+            try
+            {
+                MapGroupEditViewModelToGroup(groupEditViewModel, group);
+                _groupRepository.Add(group);
+                _groupRepository.SaveChanges();
+                TempData["message"] = $"You successfully added group {group.Name}.";
+            }
+            catch
+            {
+                TempData["error"] = "Sorry, something went wrong, the group was not created...";
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        private void MapGroupEditViewModelToGroup(GroupEditViewModel groupEditViewModel, Group group)
+        {
+            throw new NotImplementedException();
         }
     }
 }
