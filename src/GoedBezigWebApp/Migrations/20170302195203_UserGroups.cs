cf1584307921d8b2ce084813b38e0f960acdb7b5
milan dima <milandimax@gmail.com>
2017-03-02 21:12:59 +0100

    Migration for UserGroups added

---
+++ src/GoedBezigWebApp/Migrations/20170302195203_UserGroups.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GoedBezigWebApp.Migrations
+{
+    public partial class UserGroups : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
