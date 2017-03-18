a07aa3b34cc8f4040c862f8852d80926f2a0d607
Bart Vanmarcke <bart@vanmarcke-software.be>
2017-03-18 20:47:30 +0100

    Aanpassen Index

--- src/GoedBezigWebApp/Controllers/HomeController.cs
+++ src/GoedBezigWebApp/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Localization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Localization;
 using System;
+using GoedBezigWebApp.Models;
+using GoedBezigWebApp.Models.Repositories;
 
 namespace GoedBezigWebApp.Controllers
 {
@@ -10,16 +12,32 @@ namespace GoedBezigWebApp.Controllers
     {
         //added for localization
         private readonly IStringLocalizer<HomeController> _localizer;
+        private readonly IUserRepository _userRepository;
 
-        public HomeController(IStringLocalizer<HomeController> localizer)
+        public HomeController(IStringLocalizer<HomeController> localizer, IUserRepository userRepository)
         {
             _localizer = localizer;
+            _userRepository = userRepository;
         }
         //end localization
 
 
         public IActionResult Index()
         {
+            ViewData["MemberOfOrganization"] = false;
+            ViewData["MemberOfGroup"] = false;
+            if (User.Identity.IsAuthenticated)
+            {
+                User user = _userRepository.GetBy(User.Identity.Name);
+                if (user.Organization!=null)
+                {
+                    ViewData["MemberOfOrganization"] = true;
+                } else if (user.Group != null)
+                {
+                        ViewData["MemberOfGroup"] = true;
+                }
+
+            }
             return View();
         }
         //added for localization language choice handler
