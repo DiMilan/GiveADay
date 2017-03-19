d3ad78baac3c0cf917507f61a39d4603a36bcf54
Tom Vandenbussche <tom@vdbussche.net>
2017-03-19 16:32:20 +0100

    Added view for activities & events

---
+++ src/GoedBezigWebApp/Models/ActivityEventViewModels/ActivityEventViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GoedBezigWebApp.Models.ActivityEventViewModels
+{
+    public class ActivityEventViewModel
+    {
+        public ICollection<Activity> Activities { get; set; }
+        public ICollection<Event> Events { get; set; }
+
+        public ActivityEventViewModel(ICollection<Activity> activities, ICollection<Event> events)
+        {
+            Activities = activities;
+            Events = events;
+        }
+    }
+}
