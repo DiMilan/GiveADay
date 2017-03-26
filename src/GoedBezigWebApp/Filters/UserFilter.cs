4bf2f32b40b2eb3209517a474d48a8cfd9dd2c92
Tom Vandenbussche <tom@vdbussche.net>
2017-03-26 16:19:21 +0200

    Add user objec to viewbag & check if user is logged in + redirect

--- src/GoedBezigWebApp/Filters/UserFilter.cs
+++ src/GoedBezigWebApp/Filters/UserFilter.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using GoedBezigWebApp.Models.Repositories;
+using GoedBezigWebApp.Models.UserViewModels;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace GoedBezigWebApp.Filters
@@ -18,10 +20,24 @@ namespace GoedBezigWebApp.Filters
 
         public override void OnActionExecuting(ActionExecutingContext context)
         {
-            context.ActionArguments["user"] = context.HttpContext.User.Identity.IsAuthenticated
+            // Get current user
+            var user = context.HttpContext.User.Identity.IsAuthenticated
                 ? _userRepository.GetBy(context.HttpContext.User.Identity.Name)
                 : null;
-            base.OnActionExecuting(context);
+
+            var controller = context.Controller as Controller;
+
+            if (controller == null) return;
+
+            if (user != null)
+            {
+                context.ActionArguments["user"] = user;
+                controller.ViewData["User"] = new UserViewModel(user);
+            }
+            else
+            {
+                context.Result = controller.RedirectToAction("Login", "Account");
+            }
         }
     }
 }
