e2e79e57735970be3e6957ebb1e51bce3f22dad8
Tom Vandenbussche <tom@vdbussche.net>
2017-03-28 16:30:31 +0200

    Updated activity event index (accordion)

--- src/GoedBezigWebApp/Models/ActivityEventViewModels/ActivityEventViewModel.cs
+++ src/GoedBezigWebApp/Models/ActivityEventViewModels/ActivityEventViewModel.cs
@@ -7,13 +7,19 @@ namespace GoedBezigWebApp.Models.ActivityEventViewModels
 {
     public class ActivityEventViewModel
     {
-        public ICollection<Activity> Activities { get; set; }
-        public ICollection<Event> Events { get; set; }
+        public ICollection<ActivityViewModel> Activities { get; set; }
+        public ICollection<EventViewModel> Events { get; set; }
 
         public ActivityEventViewModel(ICollection<Activity> activities, ICollection<Event> events)
         {
-            Activities = activities;
-            Events = events;
+            var activityList = new List<ActivityViewModel>(activities.Count);
+            var eventList = new List<EventViewModel>(events.Count);
+
+            activityList.AddRange(activities.Select(activity => new ActivityViewModel(activity)));
+            eventList.AddRange(events.Select(@event => new EventViewModel(@event)));
+            
+            Activities = activityList.OrderBy(a => a.Title).ToList();
+            Events = eventList.OrderBy(e => e.Date).ThenBy(e => e.Title).ToList();
         }
     }
 }
---
+++ src/GoedBezigWebApp/Models/ActivityEventViewModels/ActivityViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GoedBezigWebApp.Models.ActivityEventViewModels
+{
+    public class ActivityViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public bool Accepted { get; set; }
+
+        public ActivityViewModel(Activity activity)
+        {
+            Id = activity.Id;
+            Title = activity.Title;
+            Description = activity.Description;
+            Accepted = activity.Accepted;
+        }
+    }
+}
